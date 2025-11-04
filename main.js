2e8c5f50b3042d9a307be23ae1228173aced19b9
sndp bag <sndpbagg@gmail.com>
2025-11-04 22:15:22 +0530

    final submit

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+import { createDynamicButton } from "./utility.js";
 
 
 const currentPagePath = window.location.pathname;
+if(currentPagePath.includes('home.html'))
+{
+    createDynamicButton("home","red", "btn")
+}
+else if(currentPagePath.includes('login.html'))
+{
+    createDynamicButton("Login","green","button")
+}
+
+
+
 
 console.log(currentPagePath);
